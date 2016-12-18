98e4d6386f1179e0b5cf788c3a82d4547341309d
Johannes Riedmann <johannes.riedmann@outlook.com>
2016-12-18 19:21:11 +0100

    Added mobile detector

--- functions.js
+++ functions.js
@@ -54,3 +54,11 @@ function createArray(length) {
 
     return arr;
 }
+
+function detectmob() {
+   if(window.innerWidth <= 800 && window.innerHeight <= 600) {
+     return true;
+   } else {
+     return false;
+   }
+}
